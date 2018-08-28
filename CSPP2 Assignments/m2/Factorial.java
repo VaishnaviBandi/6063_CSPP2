03fd2bf0349dbda6c34e54a6f5d0bde1d8c7e165
VaishnaviBandi <vaishnavibandi9@msitprogram.net>
2018-08-28 18:08:17 +0530

    Factorial using recursion

---
+++ CSPP2 Assignments/m2/Factorial.java
@@ -0,0 +1,13 @@
+public class Factorial{
+	public static void main(String[] args){
+		int n = 5;
+		int fact = fact(n);
+		System.out.println(fact);
+	}
+	public static int fact(int n){
+		if(n==0)
+			return 1;
+		else
+			return (n*fact(n-1));
+	}
+}
