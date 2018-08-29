628bd4409dc85a0e133507012de3ef30f28c2f2f
VaishnaviBandi <vaishnavibandi9@msitprogram.net>
2018-08-29 20:05:48 +0530

    Bigger equal smaller

---
+++ cspp2-Practice/m2/BiggerEqualSmaller.java
@@ -0,0 +1,20 @@
+public class BiggerEqualSmaller{
+ 
+	public static void main(String[] args) {
+		
+		
+		int VarA = 324;
+		int VarB = 234;
+		
+		if(VarA > VarB){
+			System.out.println("Bigger");
+		}
+		else if(VarA == VarB){
+			System.out.println("Equal");
+			
+		}
+		else{
+			System.out.println("Smaller");
+		}
+	}
+}
