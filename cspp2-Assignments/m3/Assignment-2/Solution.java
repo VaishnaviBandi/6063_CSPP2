f8c43b80be19349f61a6d90f26686936f3eac84f
VaishnaviBandi <vaishnavibandi9@msitprogram.net>
2018-08-29 18:36:37 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-Assignments/m3/Assignment-2/Solution.java
+++ cspp2-Assignments/m3/Assignment-2/Solution.java
@@ -3,29 +3,37 @@
  */
 import java.util.Scanner;
 /*.
-	Do not modify this main function.
-	*/
+    Do not modify this main function.
+    */
 /**.
-	 * Class for solution.
-	 */
+     * Class for solution.
+     */
 public class Solution {
-	private Solution() { }
-	/* Fill the main function to print the number of 7's between 1 to n*/
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		int n = s.nextInt();
-		int count = 0;
-		int ten = 10;
-		int seven = 7;
-		for (int i = 0; i <= n ; i++) {
-			int temp = i;
-			while (temp != 0) {
-				if (temp % ten == seven) {
-					count++;
-				}
-				temp = temp / ten;
-			}
-		}
-		System.out.println(count);
-	}
+    /**
+     * Constructs the object.
+     */
+    private Solution() { }
+    /* Fill the main function to print the number of 7's between 1 to n*/
+    /**
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int count = 0;
+        final int ten = 10;
+        final int seven = 7;
+        for (int i = 0; i <= n; i++) {
+            int temp = i;
+            while (temp != 0) {
+                if (temp % ten == seven) {
+                    count++;
+                }
+                temp = temp / ten;
+            }
+        }
+        System.out.println(count);
+    }
 }
