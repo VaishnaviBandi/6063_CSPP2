e1cd8cbbd37b3960a22dca1a63ca87745f9ae44e
VaishnaviBandi <vaishnavibandi9@msitprogram.net>
2018-08-29 20:12:03 +0530

    Swap

---
+++ cspp2-Practice/m1/swap.java
@@ -0,0 +1,12 @@
+public class swap{
+	public static void main(String[] args)
+	{
+		int a = 5;
+		int b = 10;
+		int c = b;
+		b = a;
+		a = c;
+		System.out.println("The swapped values are : a = " +a +" b = " +b);
+
+	}
+}
