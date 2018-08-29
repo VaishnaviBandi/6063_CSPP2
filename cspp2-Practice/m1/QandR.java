c309efc77b9bf6bd5d92fb9ae3816f9f8d31e7ed
VaishnaviBandi <vaishnavibandi9@msitprogram.net>
2018-08-29 20:13:14 +0530

    Q and R

---
+++ cspp2-Practice/m1/QandR.java
@@ -0,0 +1,11 @@
+public class QandR{
+	public static void main(String[] args)
+	{
+		int x = 2;
+		int p = 3;
+		int y = x / p;
+		int z = x % p;
+		System.out.println("Remainder : " +y);
+		System.out.println("Q :" +z);
+	}
+}
