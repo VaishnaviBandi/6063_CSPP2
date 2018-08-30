512e752eeb6dda64156bae6d8f2c305f3afec4cc
VaishnaviBandi <vaishnavibandi9@msitprogram.net>
2018-08-30 18:38:02 +0530

    first and last 6

---
+++ cspp2-Practice/m4/FirstLast6.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class FirstLast6{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int size = sc.nextInt();
+		int arr[] = new int[size];
+		for (int i=0; i<size; i++ ) {
+			arr[i] = sc.nextInt();
+		}
+		if (arr[0]==6 && arr[size-1] == 6){
+			System.out.println("True");
+			}
+		else{
+			System.out.println("False");
+		}
+		}
+    }
