0580a971b59521c62fd4467b817f3c9edd2734bb
VaishnaviBandi <vaishnavibandi9@msitprogram.net>
2018-08-30 13:34:50 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- cspp2-Assignments/m4/Assignment-1/Solution.java
+++ cspp2-Assignments/m4/Assignment-1/Solution.java
@@ -13,7 +13,7 @@ public class Solution
 		for (int i=0; i<n; i++) {
 			arr[i] = sc.nextInt();
 		}
-		for (int i=0; i<n ;i++ ) {
+		for (int i=0; i<n-1 ;i++ ) {
 			if (arr[i]>arr[i+1]){
 				large = arr[i];
 			}
