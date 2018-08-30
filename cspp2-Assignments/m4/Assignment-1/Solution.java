bd61ace12b0aa491ae40da4539b7ed83e357e2b8
VaishnaviBandi <vaishnavibandi9@msitprogram.net>
2018-08-30 13:45:25 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- cspp2-Assignments/m4/Assignment-1/Solution.java
+++ cspp2-Assignments/m4/Assignment-1/Solution.java
@@ -1,26 +1,23 @@
 
 import java.util.*;
-public class Solution
-{/*
+public class Solution {
+	/*
 	Fill this main function to print maximum of given array
 	*/
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 		int large = 0;
-		Scanner sc=new Scanner(System.in);
-		int n= sc.nextInt();
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
 		int arr[] = new int[n];
-		for (int i=0; i<n; i++) {
+		for (int i = 0; i < n; i++) {
 			arr[i] = sc.nextInt();
 		}
-		for (int i=0; i<n-1 ;i++ ) {
-			if (arr[i]>arr[i+1]){
-				large = arr[i];
-			}
-			else{
-				large = arr[i+1];
+		int max = arr[0];
+		for (int i = 0; i < n - 1 ; i++ ) {
+			if (max < arr[i]) {
+				max = arr[i];
 			}
 		}
-		
 	}
 }
+
