cf9a4dc793150a3c0653654a7599c4bb78a91eae
VaishnaviBandi <vaishnavibandi9@msitprogram.net>
2018-08-31 17:01:43 +0530

    "check_style_score -> 1 of 1 passed. style score: 0/1 "

--- cspp2-Assignments/m4/Assignment-3/Solution.java
+++ cspp2-Assignments/m4/Assignment-3/Solution.java
@@ -1,24 +1,45 @@
 
-// Given an String, Write a java method that returns the decimal value for the given binary string.
-
-
 import java.util.Scanner;
-public class Solution
-{
-	static long binaryToDecimal(String s){
-		long res = Long.parseLong(s,2);
-		return res;
-	}
-
-	public static void main(String[] args)
-	{
-		Scanner sc=new Scanner(System.in);
-		int n= sc.nextInt();
-		for(int i=0;i<=n;i++){
-			String s=sc.nextLine();
-			long res=binaryToDecimal(s);//Write binaryToDecimal function
-			System.out.println(res);
-		}
-	}
-
+/**.
+ * Class for solution.
+ */
+public class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() { }
+    /**.
+     * { function_description }
+     *
+     * @param      s     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public static String binaryToDecimal(final String s) {
+        double sum = 0;
+        for (int i = 0; i < s.length() ; i++) {
+            char c = s.charAt(i);
+            int number = Character.getNumericValue(c);
+            sum = sum + number * Math.pow(2, (s.length() - 1) - i );
+        }
+        int temp = (int) sum;
+        String res = Integer.toString(temp);
+        return res;
+    }
+    /**.
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        for (int i = 0; i < n; i++) {
+            String s = sc.nextLine();
+            String res = binaryToDecimal(s);//Write binaryToDecimal function
+            System.out.println(res);
+        }
+    }
 }
+
