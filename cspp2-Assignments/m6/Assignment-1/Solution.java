ed6dbdb1b3ed9c1039926ed036b01f4c2ec4a2ad
VaishnaviBandi <vaishnavibandi9@msitprogram.net>
2018-09-01 10:51:43 +0530

    "check_style_score -> 1 of 6 passed. style score: 0/1 "

--- cspp2-Assignments/m6/Assignment-1/Solution.java
+++ cspp2-Assignments/m6/Assignment-1/Solution.java
@@ -23,7 +23,7 @@ public final class Solution {
 			for (int j = 2; j <= i / 2; j++) {
 				if (i % j == 0) {
 					if (i % 3 == 0) {
-						System.out.println(i);
+						System.out.println(j);
 					}// end if
 				}
 			}
